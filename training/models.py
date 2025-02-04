60133bd73516a2eae6f2b09f152ff9c7fde3fe3e
GBerrow <gwilymberrow2000@gmail.com>
2025-02-04 14:45:58 +0000

    Added Achievement model under Training:
    - Stores badges earned by users
    - Tracks achievement name and date earned
    - Linked to User model for progress tracking"

--- training/models.py
+++ training/models.py
@@ -7,3 +7,12 @@ class TrainingPlan(models.Model):
 
     def __str__(self):
         return self.title
+
+class Achievement(models.Model):
+    """Stores badges earned by users"""
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    badge = models.CharField(max_length=100)  # e.g., "Steps Champion"
+    date_earned = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.badge}"
