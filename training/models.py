42b252e47f3a1d0f26bb0408847624375f3c40cc
GBerrow <gwilymberrow2000@gmail.com>
2025-02-04 14:20:01 +0000

    Added TrainingPlan model:
    - Allows users to create and manage workout plans
    
    - Stores title, description,
    and creation timestamp
    
    - Linked to User model via ForeignKey"

---
+++ training/models.py
@@ -0,0 +1,9 @@
+class TrainingPlan(models.Model):
+    """Stores user-created training plans"""
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
