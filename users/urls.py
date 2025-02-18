b06706ac12ba8a0779215890a8b001b20cd264d0
GBerrow <gwilymberrow2000@gmail.com>
2025-02-18 10:41:54 +0000

    Refactored URL Patterns:
    - Aligned path names for the necessary files.

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,18 @@
-# users/urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.login_view, name='log-in'),
-    path('signup/', views.signup_view, name='sign-up'),
+    # Profile Page (Read, Update)
+    path('profile/', views.ProfileView.as_view(), name='profile'),
+    path('profile/edit/', views.ProfileUpdateView.as_view(), name='edit_profile'),
+
+    # Settings Page (Read, Update, Delete)
+    path('settings/', views.SettingsView.as_view(), name='settings'),
+    path('settings/delete/', views.AccountDeleteView.as_view(), name='delete_account'),
+
+    # Login Page (Authenticate)
+    path('login/', views.LoginView.as_view(), name='login'),
+
+    # Signup Page (Create User)
+    path('signup/', views.SignupView.as_view(), name='signup'),
 ]
