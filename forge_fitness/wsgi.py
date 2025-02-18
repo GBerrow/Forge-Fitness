cb8bf4d606a66a8a7a2aaf8c1738286c563bef7a
GBerrow <gwilymberrow2000@gmail.com>
2025-02-18 19:21:57 +0000

    Fix WSGI path for Heroku deployment

--- forge_fitness/wsgi.py
+++ forge_fitness/wsgi.py
@@ -1,7 +1,11 @@
 import os
+import sys
 from django.core.wsgi import get_wsgi_application
 
-# Set the default settings module for the 'django' program.
+# Add the parent directory to the system path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+# Set the default settings module for the 'django' program
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'forge_fitness.settings')
 
 # Get the WSGI application
@@ -12,4 +16,3 @@ import logging
 
 logger = logging.getLogger('django')
 logger.info("WSGI application initialized for Forge Fitness")
-
