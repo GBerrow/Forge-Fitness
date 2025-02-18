c6e35a2fc51dc51724c417b493976527ab547e40
GBerrow <gwilymberrow2000@gmail.com>
2025-02-18 19:27:08 +0000

    Fix WSGI path to resolve Heroku settings module error

--- forge_fitness/wsgi.py
+++ forge_fitness/wsgi.py
@@ -2,8 +2,8 @@ import os
 import sys
 from django.core.wsgi import get_wsgi_application
 
-# Add the parent directory to the system path
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+# Add the FORGE-FITNESS directory to the system path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 # Set the default settings module for the 'django' program
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'forge_fitness.settings')
